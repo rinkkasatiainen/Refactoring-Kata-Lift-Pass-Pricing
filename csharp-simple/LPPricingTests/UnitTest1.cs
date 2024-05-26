10f805964811c8a68635367ad1d8c2f14cf2e1f1
Aki Salmi <aki.salmi@iki.fi>
2024-05-26 11:03:41 +0300

    working on test

--- csharp-simple/LPPricingTests/UnitTest1.cs
+++ csharp-simple/LPPricingTests/UnitTest1.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using System.Net.Http.Json;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 
@@ -7,12 +9,43 @@ namespace LPPricingTests;
 public class UnitTest1 : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _factory;
+
+    private readonly HttpClient _client;
     // CreatePrice("1jour", 35);
     // CreatePrice("night", 19);
 
     public UnitTest1(WebApplicationFactory<Program> factory)
     {
         _factory = factory;
+        _client = _factory.CreateClient();
+
+        CreatePrice("1jour", 35);
+        CreatePrice("night", 18);
+    }
+
+    private async Task CreatePrice(string type, int cost)
+    {
+        Dictionary<string, object> dict = new()
+        {
+            { "type", type },
+            { "cost", cost.ToString() }
+        };
+
+        HttpContent aadict = JsonContent.Create(dict);
+        var response = await _client.PutAsync($"/prices?type={type}&cost={cost.ToString()}", aadict);
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+    
+    
+    [Fact]
+    public async Task DefaultCost()
+    {
+        var response = await _client.GetAsync("/prices?type=1jour");
+        
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        throw new Exception(response.Content.ToString());
+        response.Content.ToString().Should().Be(" {\"cost\": 34}");
     }
 
     [Fact]
